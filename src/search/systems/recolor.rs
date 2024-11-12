163d63202393ebd502bc3c50c273c83dd31bfcc9
Erick Oda <erickodac@hotmail.com>
2024-11-12 13:54:26 -0300

    feat: Added Faster Path Finding Animation

--- src/search/systems/recolor.rs
+++ src/search/systems/recolor.rs
@@ -40,13 +40,27 @@ pub fn process_pending_recolor_updates(
         return;
     }
 
-    if let Some(path_with_color) = pending_updates.updates.pop_front() {
+    let quantity_of_skipped_paths = 80;
+    let mut current_path_with_color: Option<(VecDeque<Position>, Color)> = None;
+    let mut skipped_visited_positions: HashSet<Position> = HashSet::new();
+
+    for _ in 0..quantity_of_skipped_paths {
+        if let Some(path_with_color) = pending_updates.updates.pop_front() {
+            for position in &path_with_color.0 {
+                skipped_visited_positions.insert(position.clone());
+            }
+            current_path_with_color = Some(path_with_color);
+        }
+    }
+
+    if let Some(path) = current_path_with_color {
         let start = Instant::now();
         recolor_table_path(
             &mut table_with_color_and_position_query,
             &mut materials,
-            path_with_color.0,
-            path_with_color.1,
+            path.0,
+            path.1,
+            skipped_visited_positions,
         );
         let duration = start.elapsed();
         println!("Recolor path took: {:?}", duration);
@@ -77,6 +91,7 @@ fn recolor_table_path(
     materials: &mut ResMut<Assets<ColorMaterial>>,
     path: Path,
     new_color: Color,
+    skipped_visited_positions: HashSet<Position>,
 ) {
     let visited_default_color = materials.add(VISITED);
     let new_color_material = materials.add(new_color);
@@ -84,13 +99,19 @@ fn recolor_table_path(
 
     for (square_position, mut material_handle) in table_with_color_and_position_query.iter_mut() {
         if let Some(material) = materials.get_mut(&*material_handle) {
+            if path_set.contains(&square_position) {
+                let new_material = new_color_material.clone();
+                *material_handle = new_material;
+                continue;
+            }
+
             if material.color == CURRENT {
                 let new_material = visited_default_color.clone();
                 *material_handle = new_material;
             }
 
-            if path_set.contains(&square_position) {
-                let new_material = new_color_material.clone();
+            if skipped_visited_positions.contains(&square_position) {
+                let new_material = visited_default_color.clone();
                 *material_handle = new_material;
             }
         }
